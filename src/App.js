40ad8a98a7f925bebcad3c86ba61ce7047e5a8ff
moritzdueck <moritz.dueck@gmail.com>
2020-06-22 17:05:50 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,44 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { items } from './data';
+import CSVParser from './util/CSVParser';
 
 function App() {
+
+  const [csvData, setCsvData] = useState([]);
+  const weberUndOttEANs = RegExp('4030197.*|4034082.*|4037886.*|4042886.*|4043925.*|4044636.*|4045817.*|4046886.*|4048459.*|4049333.*|4052049.*|4052847.*|4056244.*|4062795.*|4062796.*');
+
+  useEffect(() => {
+    document.addEventListener('click', refocusScannerInput)
+    fetchCSVData()
+  }, [])
+
+  async function fetchCSVData() {
+    let csvParser = new CSVParser();
+    let data = await csvParser.getDataFromCSV();
+    console.log("The following data is used for processing:");
+    console.log(data);
+    setCsvData(data)
+  }
+
   return (
     <div className="App">
-      <p id='number'> </p>
+      <p id='scanned_EAN'>Scan to Start</p>
       <form className='hide'>
-        <input autofocus="true" type='text' id='scanner_result' onInput={processBarcode} />
+        <input autoFocus={true} type='text' id='scanner_input' onInput={processEAN} />
       </form>
-      <div id='result'/>
+      <div id='result' />
     </div>
   );
-}
 
-
-const regex = RegExp('4030197.*|4034082.*|4037886.*|4042886.*|4043925.*|4044636.*|4045817.*|4046886.*|4048459.*|4049333.*|4052049.*|4052847.*|4056244.*|4062795.*|4062796.*');
-function isInRange(ean) {
-  return regex.test(ean)
-}
-
-function processBarcode(event) {
-
-  let code = event.target.value;
-  console.log("Barcode detected and processed : [" + code + "]");
-  if (!isInRange(code)) {
-    return
-  }
-  if (items.includes(code)) {
-    console.log("SUCCESS");
-    document.getElementById('result').style.backgroundColor = 'green'
-    document.getElementById('number').innerHTML = code
-  } else {
-    console.log("FAILURE");
-    document.getElementById('result').style.backgroundColor = 'red'
-    document.getElementById('number').innerHTML = code
+  function processEAN(event) {
+    let ean = event.target.value;
+    console.log("Barcode detected and processed : [" + ean + "]");
+    if (!isWeberUndOttEan(ean)) {
+      warnUserOfWrongEAN(ean);
+      return
+    }
+    showScannedEAN(ean);
+    lookForEANinCSVData(ean);
+    resetScannerInputField();
   }
 
-  setTimeout(() => { document.getElementById('scanner_result').value = '' }, 200)
+  function isWeberUndOttEan(ean) {
+    return weberUndOttEANs.test(ean) && ean.length === 13
+  }
+
+  function warnUserOfWrongEAN(ean) {
+    document.getElementById('result').style.backgroundColor = '#ffc800';
+    document.getElementById('scanned_EAN').innerHTML = 'EAN-Fehler: ' + ean;
+    setTimeout(() => { document.getElementById('scanner_input').value = ''; }, 200);
+  }
+
+
+  function showScannedEAN(ean) {
+    document.getElementById('scanned_EAN').innerHTML = formatEAN(ean);
+  }
+
+  function formatEAN(ean) {
+    if (ean.length !== 13) {
+      return
+    }
+    let asString = '' + ean
+    return asString.substring(0, 1)
+      + " " + asString.substring(1, 7)
+      + " " + asString.substring(7)
+  }
+
+  function lookForEANinCSVData(ean) {
+    let foundAtIndex = csvData.findIndex(e => e.EAN === ean);
+    const wasFound = foundAtIndex !== -1;
+    if (wasFound) {
+      let entry = csvData[foundAtIndex];
+      document.getElementById('result').style.backgroundColor = (entry.color === '' ? '#32a852' : entry.color);
+    }
+    else {
+      document.getElementById('result').style.backgroundColor = 'red';
+    }
+  }
+
+
+  function resetScannerInputField() {
+    setTimeout(() => { document.getElementById('scanner_input').value = ''; }, 200);
+  }
 
 }
 
 export default App;
+
+function refocusScannerInput() {
+  document.getElementById('scanner_input').setAttribute('readonly', 'readonly');
+  setTimeout(() => {
+    document.getElementById('scanner_input').focus();
+    document.getElementById('scanner_input').removeAttribute('readonly');
+  }, 50);
+}
---
+++ src/util/CSVParser.js
@@ -0,0 +1,19 @@
+import csv from "csvtojson";
+
+export default class CSVParser {
+    getDataFromCSV() {
+        return fetch('data.csv').then(function (response) {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder('utf-8');
+
+            return reader.read().then(function (result) {
+                let data = decoder.decode(result.value);
+                return csv()
+                    .fromString(data)
+                    .then((jsonObj) => {
+                        return jsonObj;
+                    })
+            });
+        });
+    }
+}
