d73723c921598f7fdd77105d64aa20f811d4ad43
moritzdueck <moritz.dueck@gmail.com>
2020-07-23 07:58:23 +0200

    fixed parser

--- src/util/CSVParser.js
+++ src/util/CSVParser.js
@@ -1,21 +1,10 @@
 import csv from "csvtojson";
 
 export default class CSVParser {
-    getDataFromCSV() {
-        return fetch('data.csv').then(function (response) {
-            let reader = response.body.getReader();
-            let decoder = new TextDecoder('utf-8');
-
-            return reader.read().then(function (result) {
-                let data = decoder.decode(result.value);
-                return csv({
-                    delimiter:';'
-                })
-                    .fromString(data)
-                    .then((jsonObj) => {
-                        return jsonObj;
-                    })
-            });
-        });
+    async getDataFromCSV() {
+        let response = await fetch('data.csv');
+        let text = await response.text();
+        let result = await csv({ delimiter: ';' }).fromString(text);
+        return result;
     }
 }
